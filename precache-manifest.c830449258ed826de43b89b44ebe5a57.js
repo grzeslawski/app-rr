62c336665deffb7d80657ef5e64d1cfcd86445a4
Jarosław Grzesik <jaroslaw.grzesik94@gmail.com>
2020-01-27 21:46:17 +0100

    Updates

---
+++ precache-manifest.c830449258ed826de43b89b44ebe5a57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fae63a7bd48e3c254dd",
+    "url": "/app-rr/static/css/main.00400d1d.chunk.css"
+  },
+  {
+    "revision": "4fae63a7bd48e3c254dd",
+    "url": "/app-rr/static/js/main.4fae63a7.chunk.js"
+  },
+  {
+    "revision": "9873500cbc742282ea50",
+    "url": "/app-rr/static/js/runtime~main.9873500c.js"
+  },
+  {
+    "revision": "d6c3ff2124a769703257",
+    "url": "/app-rr/static/js/2.d6c3ff21.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "49592a3325f4173e7d5ca0894bb4023e",
+    "url": "/app-rr/index.html"
+  }
+];
